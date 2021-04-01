5976fed3b84d742fc02929c42d15327f927d9618
kaverichunduru <81732854+kaverichunduru@users.noreply.github.com>
2021-04-01 10:28:35 +0530

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,24 @@
+class X
+{
+public void display()
+{
+System.out.println("class X");
+}
+}
+class Y extends X
+{
+public void display()
+{
+System.out.println("class Y");
+}
+}
+class overloading
+{
+public static void main(String a[])
+{
+X X1=new Y();
+X1.display();
+}
+}
+
+

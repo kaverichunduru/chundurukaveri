40d78a3ec060f0609af506ef1bd981acdebf332b
kaverichunduru <81732854+kaverichunduru@users.noreply.github.com>
2021-04-01 10:26:56 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,16 @@
+class greatest
+{
+public static void main(String args[])
+{
+int a,b,c;
+a=30;
+b=20;
+c=10;
+if(a>b&&a>c)
+System.out.println("the largest number is="+a);
+else if(b>a&&b>c)
+System.out.println("the largest number is="+b);
+else
+System.out.println("the largest number is="+c);
+}
+}
